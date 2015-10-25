28e95b9e243cfb1a6dc5b447d00f5bd27a3954fd
euhmeuh <euhmeuh@vivaldi.net>
2015-10-25 19:28:19 +0100

    Bump 0.2

--- rilouw/settings.py
+++ rilouw/settings.py
@@ -18,7 +18,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import dj_database_url
 
-APP_VERSION = '0.1'
+APP_VERSION = '0.2'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
