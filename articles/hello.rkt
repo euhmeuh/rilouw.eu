32defc3846eff850ae49b7f566410be3ca31db32
euhmeuh <euhmeuh@vivaldi.net>
2018-02-04 20:51:38 +0100

    show tags and "Back" button on article pages

--- articles/hello.rkt
+++ articles/hello.rkt
@@ -5,4 +5,4 @@
     "Hello world! I'm Jérôme, professional developer and hacker." (n) (n)
     "This is my brand new blog where I talk about " 'slow-tech ", " 'DIY " projects, " 'feminism ", " 'ecology ", metaphysical aspects of life..." (n) (n)
     "I wanted to make a blog from scratch using " 'Racket ", to see how simple and stupid you can keep a web server when using a " 'lisp " language. " (n) (n)
-    "Feel free to pick up whatever you please, get inspired, and share your thought around."))
+    "Feel free to pick up whatever you please, get inspired, and share your thoughts around."))
--- pages/article.rkt
+++ pages/article.rkt
@@ -4,13 +4,21 @@
   article-page)
 
 (require
+  racket/list
   "_base.rkt"
   "../entities/blog.rkt")
 
 (define (article-page db article)
+  (define back-home-link
+    (paragraph (link "Back home" "/")))
+  (define tags-list
+    (apply paragraph (cons "Tags: "
+                           (add-between (article-tags article) ", "))))
   (base-page db (article-title article) '()
     (lambda ()
       `(main
          (article
            (h2 ([id ,(article-id article)]) ,(article-title article))
-           ,@(render-elements article))))))
+           ,@(render-elements article)
+           ,(render-element tags-list)
+           ,(render-element back-home-link))))))
