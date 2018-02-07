c8f4992a55f0872f47425cb27c78b5a2d54b03e5
euhmeuh <euhmeuh@vivaldi.net>
2018-02-07 23:51:20 +0100

    first article

--- articles/hello.rkt
+++ articles/hello.rkt
@@ -2,7 +2,46 @@
 
 (article "Hello world!" '(english meta)
   (paragraph
-    "Hello world! I'm Jérôme, professional developer and hacker." (n) (n)
-    "This is my brand new blog where I talk about " 'slow-tech ", " 'DIY " projects, " 'feminism ", " 'ecology ", metaphysical aspects of life..." (n) (n)
-    "I wanted to make a blog from scratch using " 'Racket ", to see how simple and stupid you can keep a web server when using a " 'lisp " language. " (n) (n)
+    "Hello world! I'm Jérôme, professional developer and hacker." (n)
+    (n)
+    "This is my brand new blog where I talk about " 'slow-tech ", " 'DIY " projects, " 'feminism ", " 'ecology ", metaphysical aspects of life..." (n)
+    (n)
+    "Technically speaking, I wanted to make a blog from scratch using " 'Racket ", to see how simple "
+    "and stupid you can keep a web server when using a " 'lisp " language." (n)
+    (n)
+    "Most importantly, I wanted to write about things that please and displease me in this world.")
+  (paragraph
+    "My head spins every time I stumble upon a new evidence that humans are brutal, ruthless and unable to live together, "
+    "while in the same time being forced by their very nature to actually live together.")
+  (paragraph
+    "In order to understand why I am touched by this aching feeling, I wanna write about every thing that triggers it. "
+    "I want to talk about " 'work ", especially work in the IT industry, and how we are pointlessly building "
+    "hopes in machines to save us, as if it was a new bio-mechanical god.")
+  (paragraph
+    "I want to talk about being a white european man with full " 'privileges " in a society that pushes me to destroy others and aim for the top.")
+  (paragraph
+    "I want to talk about people convinced that we are building a better world "
+    "while we are in fact shitting our pants and trying random stuff (alas it is not always a bad thing).")
+  (paragraph
+    "I also wanted to write about stuff I like or love in this world, "
+    "because I want to focus my life on those and I can't find them if I don't put words on them.")
+  (paragraph
+    "I want to talk about taking it slow, or how you can have fun building small and environment-friendly computers (see the " 'slow-tech " tag).")
+  (paragraph
+    "I want to talk about how the 'hottest new technology available' is far from being artificial intelligence nor the latest smartphone on the market, "
+    "but discoveries made in the 60's by passionate programmers and thinkers. "
+    "I want to stress out to the IT croud around the fact that IT is moving SLOW, very, very SLOW. "
+    "So please let the hype pass by, and focus on what's essential and really makes the core of your job.")
+  (paragraph
+    "I want to talk about relationships, love, how difficult it is to study human relations, and how wonderful the world would be if we understood them better.")
+  (paragraph
+    "I want to talk about preserving the one and only planet we have, and get humanity into a sane relationship with it. "
+    "We're plunging towards an ecological recession, and the more we are prepared, the less painfull it'll be (especially if we understand better how to live together)." (n)
+    "This is why I think studying love, sex, gender, and other social constructions is deeply linked to ecology, "
+    "our interactions with the planet, and the management and regulation of our waste." (n)
+    "If we don't understand ourselves, we cannot understand nor question our place on Earth.")
+  (paragraph
+    "It won't immediately be good, well written and documented, but I hope this blog will bring you confidence, "
+    "will make you want to fight for you rights, and hopefully will make you learn a thing or two.")
+  (paragraph
     "Feel free to pick up whatever you please, get inspired, and share your thoughts around."))
