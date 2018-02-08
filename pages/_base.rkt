9007347ae4d4e3f9719dac659d6ea5b0dc8c9da9
euhmeuh <jerome.martin@corp.ovh.com>
2018-02-08 17:31:35 +0100

    add metadata to the page & a favicon

--- pages/_base.rkt
+++ pages/_base.rkt
@@ -1,9 +1,13 @@
 #lang racket/base
 
 (provide
-  base-page)
+  base-page
+  page-description
+  page-author
+  page-keywords)
 
 (require
+  racket/string
   "_footer.rkt"
   "../entities/blog.rkt")
 
@@ -11,6 +15,29 @@
   (list
     (link "Hot topics" "#topics")
     (link "About" "#about")))
+
+(define page-description (make-parameter "Jérôme's blog about slow-tech, feminism & ecology."))
+(define page-author (make-parameter "Jérôme Martin"))
+(define page-keywords
+  (make-parameter
+    (string-join
+      (list
+        "slow tech"
+        "feminism"
+        "ecology"
+        "jerome martin"
+        "jérôme martin"
+        "information technology"
+        "IT"
+        "technology"
+        "lisp"
+        "scheme"
+        "racket"
+        "programming"
+        "programmer"
+        "dev"
+        "development")
+      ", ")))
 
 (define (render-navigation links)
   `(nav (ul ,@(map (lambda (link)
@@ -26,8 +53,13 @@
        (meta ([charset "utf-8"]))
        (meta ([name "viewport"]
               [content "width=device-width, initial-scale=1.0, shrink-to-fit=no"]))
-       ,(render-title title)
-       (link ([rel "stylesheet"] [type "text/css"] [href "/common.css"])))
+       (meta ([name "description"] [content ,(page-description)]))
+       (meta ([name "keywords"] [content ,(page-keywords)]))
+       (meta ([name "author"] [content ,(page-author)]))
+       (link ([rel "icon"] [href "/favicon.ico"]))
+       (link ([rel "shortcut icon"] [href "/favicon.ico"]))
+       (link ([rel "stylesheet"] [type "text/css"] [href "/common.css"]))
+       ,(render-title title))
      (body
        (header (h1 "Rilouw.eu"))
        ,(render-navigation (cons (link "Home" "/")
