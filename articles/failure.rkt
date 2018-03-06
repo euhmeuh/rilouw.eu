e033dc3a4fe3e9db3c52f9bce4d5a038920aab09
euhmeuh <euhmeuh@vivaldi.net>
2018-03-06 18:34:27 +0100

    remove dummy articles

--- articles/failure.rkt
+++ articles/failure.rkt
@@ -1,32 +1,42 @@
-#lang s-exp "../database/article-lang.rkt"
+#lang at-exp s-exp "../database/article-lang.rkt"
 
-(article "Le rapport à l'erreur"
-         (pubdate 2018 2 15)
-         '(draft french workplace human-rights failure error agile methodology unit-tests tests TDD)
-  (paragraph
-    "L'erreur doit être un moteur, un moyen d'expression."
-    "Ne plus avoir peur de ses erreurs (grâce à des «filets de protection» humains et techniques), "
-    "c'est libérer son expressivité et permettre à l'innovation d'avoir lieu.")
-  (fold)
-  (paragraph
-    "Les enfants son créatifs car on créé pour eux un environnement où ils peuvent expérimenter sans craintes. "
-    "Les scientifiques sont créatifs car ils oublient de faire des choses «utiles» (comprendre «rentables») et se concentrent sur «essayer ce qui n'a pas encore été essayé». "
-    "Chez Google, 9 projets sur 10 échouent.")
-  (section "Chez les devs"
-    (dotted-list
-      "Unit tests"
-      "Preuve formelle"
-      "Méthodes agiles"
-      "Cycle d'amélioration continue"))
-  (section "Dans la vie"
-    (paragraph "Le droit à l'erreur")
-    (paragraph "Quid des erreurs préjudiciables ?"
-               "Toutes les erreurs sont-elles pardonnables ?")
-    (paragraph "Comment créer une société qui authorise l'erreur?"))
-  (section "À recouper avec"
-    (paragraph "Toutes les populations ont-elles autant le droit à l'erreur ? Les femmes ? Les noir·e·s ? Les pauvres ?"))
-  (section "Références"
-    (dotted-list
-      (link "Vision de l'erreur à travers le monde (France/Japon/USA/...)" "#")
-      (link "Évolution des tribunaux" "#")
-      (link "Évolution des établissements pénitentiers" "#"))))
+@article["Le rapport à l'erreur"
+         @pubdate[2018 3 2]
+         @'(french workplace human-rights failure error agile methodology unit-tests tests TDD)
+
+@p{L'erreur doit être un moteur, un moyen d'expression.
+   Ne plus avoir peur de ses erreurs (grâce à des «filets de protection» humains et techniques),
+   c'est libérer son expressivité et permettre à l'innovation d'avoir lieu.}
+
+@(fold)
+
+@p{Les enfants son créatifs car on créé pour eux un environnement où ils peuvent expérimenter sans craintes.}
+
+@p{Les scientifiques sont créatifs car ils oublient de faire des choses «utiles» (comprendre «rentables»)
+   et se concentrent sur «essayer ce qui n'a pas encore été essayé».}
+
+@p{Chez Google, 9 projets sur 10 échouent.}
+
+@section["Chez les devs"
+  @dotted-list[
+    "Unit tests"
+    "Preuve formelle"
+    "Méthodes agiles"
+    "Cycle d'amélioration continue"]]
+
+@section["Dans la vie"
+
+  @p{Le droit à l'erreur}
+  @p{Quid des erreurs préjudiciables ?
+     Toutes les erreurs sont-elles pardonnables ?}
+  @p{Comment créer une société qui authorise l'erreur ?}
+  @p{La prison en France est un outil de dissuasion (comme le reste des forces de l'ordre ?) plutôt que d'être un lieu d'accueil}]
+
+@section["À recouper avec"
+  @p{Toutes les populations ont-elles autant le droit à l'erreur ? Les femmes ? Les noir·e·s ? Les pauvres ?}]
+
+@section["Références"
+  @dotted-list[
+    @link["Vision de l'erreur à travers le monde (France/Japon/USA/...)" "#"]
+    @link["Évolution des tribunaux" "#"]
+    @link["Évolution des établissements pénitentiers" "#"]]]]
