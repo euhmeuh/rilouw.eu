f2fbcbc303f3cfbf71ef5968e182e016d5a71081
euhmeuh <euhmeuh@vivaldi.net>
2018-03-10 19:10:12 +0100

    write a better footer

--- pages/_footer.rkt
+++ pages/_footer.rkt
@@ -17,8 +17,10 @@
      (h2 ([id "topics"]) "Hot topics")
      ,(render-frequent-tags (send db get-frequent-tags #:at-least 1))
      (h2 ([id "about"]) "About this blog")
-     (p "I learn new things everyday, and I like to put up great and useless theories about the inner workings of the world.")
-     (p "You'll find them here.")
+     (p "Hey! I'm Jérôme, feminist hacker.")
+     (p "I learn new things everyday, and I like to put up great "
+        "and sometimes useless theories about the inner workings of the world, which you'll find here.")
+     (p "This blog is mostly about feminism, ecology and hacking. Please read and enjoy responsibly.")
      (hr)
      (p "Copyright © Jérôme Martin - All rights reserved.")
      (p ([class "small"])
