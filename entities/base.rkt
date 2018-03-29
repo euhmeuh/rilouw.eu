fe7651103ee65e54d4e92c06043d6a0a9d10e97b
euhmeuh <euhmeuh@vivaldi.net>
2018-03-29 15:10:55 +0200

    use the #:with option for macros

--- entities/base.rkt
+++ entities/base.rkt
@@ -42,14 +42,14 @@
     (pattern name:id))
   (syntax-parse stx
     [(_ nmp:name-maybe-parent (field ...) body ...)
-     (with-syntax ([function-name (format-id stx "render-~a" #'nmp.name)])
-       #`(begin
-           (struct #,@ #'nmp (field ...)
-            #:methods gen:renderer
-            [(define (render nmp.name)
-              ((function-name) nmp.name))])
-           (define function-name
-             (make-parameter (lambda (nmp.name) body ...)))))]))
+     #:with function-name (format-id stx "render-~a" #'nmp.name)
+     #`(begin
+         (struct #,@ #'nmp (field ...)
+          #:methods gen:renderer
+          [(define (render nmp.name)
+            ((function-name) nmp.name))])
+         (define function-name
+           (make-parameter (lambda (nmp.name) body ...))))]))
 
 (define (render-element element)
   (cond
